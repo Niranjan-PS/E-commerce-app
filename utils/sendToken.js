bed1f1b4dd87c34ae9002a8b50ff8b0e8fd74f5d
niranjanps <niranjanpsniranjan0@gmail.com>
2025-06-13 08:44:16 +0530

    updated sendtoken fn

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -2,7 +2,7 @@ export const sendToken = (user, res, tokenName = 'userToken') => {
   const token = user.generateToken();
 
   const options = {
-    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'strict',
